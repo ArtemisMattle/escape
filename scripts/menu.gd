f88c34d72af30594c0f56e3ebd6e5a7e0365dfce
ArtemisMattle <leander.mattle@outlook.com>
2025-07-30 18:52:40 +0200

    credits added

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
 @onready var settings: Control = $Settings
+@onready var credits: Control = $Credits
 
 
 func _on_settings_pressed() -> void:
 	settings.visible = true
+
+
+func _on_credits_pressed() -> void:
+	credits.visible = true
+
+func _on_return_pressed() -> void:
+	credits.visible = false
